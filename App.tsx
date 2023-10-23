ee2a219524ac82f5507984d5d7cc6c8b44abe6d8
Shayanta Shopnil <shopnil710@gmail.com>
2023-10-23 20:08:20 +0600

    init commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SearchScreen from "./src/screens/SearchScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+const stack = createStackNavigator();
+
+const App = () =>{
+
+
+    return(
+        <NavigationContainer>
+            <stack.Navigator>
+                <stack.Screen name="Business Search" component={SearchScreen}/>
+            </stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default App;
